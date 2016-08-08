ffee19d1a11c2286af4c400e025c328d41f6b5e8
Sanderson Oliveira de Macedo <sandecom@gmail.com>
2016-08-08 19:36:08 -0300

    Criação da MainActivity

--- app/src/main/java/sandeco/bluetooth/MainActivity.java
+++ app/src/main/java/sandeco/bluetooth/MainActivity.java
@@ -1,28 +1,14 @@
 package sandeco.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
